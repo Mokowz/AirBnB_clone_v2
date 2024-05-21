62980ecfe64f371b7b4bf87391091430cb336082
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-21 16:17:51 +0300

    Run a minimal application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Flask Application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "<p>Hello HBNB!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
