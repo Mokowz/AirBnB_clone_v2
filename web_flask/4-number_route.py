5d37842373d6b2d5e9d5fa968b9121671b02b03c
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-21 16:48:35 +0300

    Handle int parameters

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Run a Flask App
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def another(text='is cool'):
+    text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
